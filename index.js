e7ffabfbcbfef534e2703bc3096e76e2774de605
SaurusXI <vermashantanu@hotmail.com>
2020-10-10 20:48:06 +0530

    start

---
+++ index.js
@@ -0,0 +1,35 @@
+btn = document.getElementById('hearButton');
+btn.addEventListener("click",
+    function send_request(){
+        console.log('here');
+        const URL = "http://localhost:5000/api/test";
+        var choice = document.getElementById("t2");
+        var speaker = choice.options[choice.selectedIndex].value;
+        const data={
+            target_text : document.getElementById("t1").value,
+            target_speaker : speaker
+        };
+
+
+        const params = {
+            body : JSON.stringify(data),
+            method : "POST",
+            headers : {
+                "content-type" : "application/json; charset=UTF-8"
+            }
+        };
+
+        console.log("Sending info to the backend: ", params);
+        
+        fetch(URL, params)
+            .then(async (res) => {
+                r = res
+                document.getElementById("audio_test").setAttribute("src", "http://localhost:5000/api/test/audio")
+                document.getElementById("audio_test").setAttribute("type", "audio/mp3")
+                let music = document.getElementById('music')
+                await music.load()
+                music.play()
+            }
+        );
+    }
+);
